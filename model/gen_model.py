878dd131012f0c0a63f9ba932f0a5d45bcf20e4b
Zhao_Aaron_azhao96 <azhao96@ME01L2509LAB07.me.gatech.edu>
2024-03-01 19:04:51 -0500

    investigate the transformer

--- model/gen_model.py
+++ model/gen_model.py
@@ -232,9 +232,10 @@ class GPT(nn.Module):
         the sequence max_new_tokens times, feeding the predictions back into the model each time.
         Most likely you'll want to make sure to be in model.eval() mode of operation for this.
         """
+        idx_tot = []
         for _ in range(max_new_tokens):
             # if the sequence context is growing too long we must crop it at block_size
-            idx_cond = idx if idx.size(1) <= self.block_size else idx[:, -self.block_size:]
+            idx_cond = idx if idx.size(1) <= self.block_size else idx[:, -self.block_size:, :]
             # forward the model to get the logits for the index in the sequence
             logits, _ = self(idx_cond)
             # pluck the logits at the final step and scale by desired temperature
@@ -254,5 +255,6 @@ class GPT(nn.Module):
             # append sampled index to the running sequence and continue
             # print(idx.shape, idx_next.shape)
             idx = torch.cat((idx, idx_next), dim=1)
+            idx_tot.append(idx_next.cpu())
 
-        return idx
+        return idx, idx_tot
