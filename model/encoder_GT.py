f57b02bbe33f52b6bc5b59d4e7ae793b0b1f63b5
Zhao_Aaron_azhao96 <azhao96@ME01L2509LAB07.me.gatech.edu>
2024-03-24 19:20:03 -0400

    add bert and get training loop running

--- model/encoder_GT.py
+++ model/encoder_GT.py
@@ -25,78 +25,12 @@ class NewGELU(nn.Module):
     def forward(self, x):
         return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))
 
-class CausalSelfAttention(nn.Module):
-    """
-    A vanilla multi-head masked self-attention layer with a projection at the end.
-    It is possible to use torch.nn.MultiheadAttention here but I am including an
-    explicit implementation here to show that there is nothing too scary here.
-    """
-
-    def __init__(self, n_layer, n_head, n_embd, vocab_size, block_size, pdrop=0.1, device='cpu'):
-        super().__init__()
-        assert n_embd % n_head == 0
-        # key, query, value projections for all heads, but in a batch
-        self.c_attn = nn.Linear(n_embd, 3 * n_embd, device=device)
-        # output projection
-        self.c_proj = nn.Linear(n_embd, n_embd, device=device)
-        # regularization
-        self.attn_dropout = nn.Dropout(pdrop)
-        self.resid_dropout = nn.Dropout(pdrop)
-        # causal mask to ensure that attention is only applied to the left in the input sequence
-        self.register_buffer("bias", torch.tril(torch.ones(block_size, block_size, device=device))
-                                     .view(1, 1, block_size, block_size))
-        self.n_head = n_head
-        self.n_embd = n_embd
-
-    def forward(self, x):
-        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
-
-        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        q, k ,v  = self.c_attn(x).split(self.n_embd, dim=2)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-
-        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
-        att = F.softmax(att, dim=-1)
-        att = self.attn_dropout(att)
-        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
-
-        # output projection
-        y = self.resid_dropout(self.c_proj(y))
-        return y
-
-class Block(nn.Module):
-    """ an unassuming Transformer block """
-
-    def __init__(self, n_layer, n_head, n_embd, vocab_size, block_size, pdrop=0.1, device='cpu'):
-        super().__init__()
-        self.ln_1 = nn.LayerNorm(n_embd, device=device)
-        self.attn = CausalSelfAttention(n_layer, n_head, n_embd, vocab_size, block_size, pdrop=0.1, device=device)
-        self.ln_2 = nn.LayerNorm(n_embd, device=device)
-        self.mlp = nn.ModuleDict(dict(
-            c_fc    = nn.Linear(n_embd, 4 * n_embd, device=device),
-            c_proj  = nn.Linear(4 * n_embd, n_embd, device=device),
-            act     = NewGELU(),
-            dropout = nn.Dropout(pdrop),
-        ))
-        m = self.mlp
-        self.mlpf = lambda x: m.dropout(m.c_proj(m.act(m.c_fc(x)))) # MLP forward
-
-    def forward(self, x):
-        x = x + self.attn(self.ln_1(x))
-        x = x + self.mlpf(self.ln_2(x))
-        return x
-    
 class BertBlock(nn.Module):
     """ Feed forward block """
 
     def __init__(self, n_head, n_embd, pdrop=0.1, device='cpu'):
         super().__init__()
-        self.attention = nn.MultiheadAttention(n_embd, n_head)
+        self.attention = nn.MultiheadAttention(n_embd, n_head, device=device)
         self.feed_forward = nn.ModuleDict(dict(
             c_fc    = nn.Linear(n_embd, 4 * n_embd, device=device),
             c_proj  = nn.Linear(4 * n_embd, n_embd, device=device),
@@ -113,11 +47,11 @@ class BertBlock(nn.Module):
         ))
         self.dropout = nn.Dropout(p=pdrop)
         m = self.feed_forward
-        self.feed_forward_f = lambda x: m.dropout(m.c_proj(m.act(m.c_fc(x))))
-        self.layer_f = lambda x, forward, sublayer: x + sublayer.dropout(forward(sublayer.norm(x)))
+        self.feed_forward_f = lambda _x: m.dropout(m.c_proj(m.act(m.c_fc(_x))))
+        self.layer_f = lambda _x, forward, sublayer: _x + sublayer.dropout(forward(sublayer.norm(_x)))
 
-    def forward(self, x, mask):
-        x = self.layer_f(x, lambda _x: self.attention.forward(_x, _x, _x, mask=mask), self.input_sublayer)
+    def forward(self, x):
+        x = self.layer_f(x, lambda _x: self.attention.forward(_x, _x, _x)[0], self.input_sublayer)
         x = self.layer_f(x, self.feed_forward_f, self.output_sublayer)
         return self.dropout(x)
 
@@ -178,8 +112,11 @@ class Encoder_GPT(nn.Module):
             x = block(x)
         x = self.transformer.ln_f(x)
         logits = self.lm_head(x)
-        # print(logits.shape)
-        return logits
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+
+        return logits, loss
 
     @torch.no_grad()
     def generate(self, idx, max_new_tokens, temperature=1.0, do_sample=False, top_k=None):
